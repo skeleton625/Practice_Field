3e896698bfb0b4d4648c69937dbd6e370d820008
skeleton625 <chaos0824@naver.com>
2021-02-24 20:33:49 +0900

    밭 정보 스크립트 구현

---
+++ Assets/Scripts/FieldData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Field Data", menuName = "Datas/FieldData")]
+public class FieldData : ScriptableObject
+{
+    public int[] FarmingCount = null;
+    public float[] FarmingScale = null;
+    public float GrassScale;
+}
