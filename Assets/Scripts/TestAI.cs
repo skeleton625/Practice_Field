32e5da7a456383a22d71aa29b2e540db923b86cf
skeleton625 <chaos0824@naver.com>
2021-02-24 20:33:15 +0900

    테스트용 AI 추가

---
+++ Assets/Scripts/TestAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class TestAI : MonoBehaviour
+{
+    [SerializeField] private float workScale = 0f;
+
+    private NavMeshAgent agent = null;
+
+    public float WorkScale{ get => workScale; }
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetDestination(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
