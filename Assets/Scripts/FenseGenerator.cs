86f7c2d25c3977b352073fc165bb00551440b148
skeleton625 <chaos0824@naver.com>
2021-03-15 19:07:48 +0900

    밭 연결 기능 추가, 버그 수정(밭 내 기둥 설치, 울타리 밭 가로지르기 등)

--- Assets/Scripts/FenseGenerator.cs
+++ Assets/Scripts/FenseGenerator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class FenseGenerator : MonoBehaviour
 {
-    [SerializeField] private float LengthLimit = 0f;
+    [SerializeField] private float DefaultLengthLimit = 0f;
     [SerializeField] private float CropsAlpha = 1f;
     [SerializeField] private float CropsScale = .1f;
     [SerializeField] private Transform Fense = null;
@@ -16,23 +16,27 @@ public class FenseGenerator : MonoBehaviour
     [SerializeField] private LayerMask RayMask = default;
     [SerializeField] private Color EnableColor = Color.black;
     [SerializeField] private Color DisableColor = Color.black;
+    [SerializeField] private ObjectCollider FenseCollider = null;
 
     private int poleCount = 0;
-    private float lenLimit = 0f;
+    private int connectCount = 0;
+    private float lengthLimit = 0f;
     private bool isEnable = true;
-    private bool isCollid = false;
+    private bool isConnect = false;
+    private Vector3 colliderScale = Vector3.zero;
 
     private Camera mainCamera = null;
     private CropsEntity preCropsEntity = null;
-    private CropsEntity collidCropsEntity = null;
     private List<Transform> poleTrans = null;
 
     private void Start()
     {
         mainCamera = Camera.main;
         poleTrans = new List<Transform>();
-        LengthLimit = LengthLimit * LengthLimit;
         VisualMaterial.SetColor("_BaseColor", EnableColor);
+        colliderScale = FenseCollider.transform.localScale;
+        DefaultLengthLimit = DefaultLengthLimit * DefaultLengthLimit;
+        lengthLimit = DefaultLengthLimit;
     }
 
     // Update is called once per frame
@@ -42,34 +46,49 @@ public class FenseGenerator : MonoBehaviour
         if(Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 1000f, RayMask))
         {
             transform.position = hit.point;
+            var flag = isConnect || FenseCollider.IsUnCollid;
             if (poleCount > 0)
             {
-                var flag = IsBelowLimit(poleTrans[poleCount - 1].position, hit.point);
+                flag &= IsBelowLimit(poleTrans[poleCount - 1].position, hit.point);
                 if (poleCount.Equals(3))
-                    flag &= IsBelowLimit(hit.point, poleTrans[0].position);
-                if (isEnable != flag)
                 {
-                    isEnable = flag;
-                    if (flag)
-                        VisualMaterial.SetColor("_BaseColor", EnableColor);
-                    else
-                        VisualMaterial.SetColor("_BaseColor", DisableColor);
+                    float angle = 0;
+                    angle += CalculateAngle(poleTrans[0].position, poleTrans[1].position, poleTrans[2].position);
+                    angle += CalculateAngle(poleTrans[1].position, poleTrans[2].position, hit.point);
+                    angle += CalculateAngle(poleTrans[2].position, hit.point, poleTrans[0].position);
+                    angle += CalculateAngle(hit.point, poleTrans[0].position, poleTrans[1].position);
+                    flag &= IsBelowLimit(hit.point, poleTrans[0].position) && angle > 350;
+                    if (isConnect && (connectCount % 2).Equals(0))
+                        flag = false;
                 }
+                FenseCollider.transform.position = Vector3.Lerp(poleTrans[poleCount - 1].position, hit.point, .05f);
+                colliderScale.z = Vector3.Distance(FenseCollider.transform.position, hit.point) * 9.5f;
+                FenseCollider.transform.localScale = colliderScale;
+                FenseCollider.transform.LookAt(hit.point);
+            }
+            if (isEnable != flag)
+            {
+                isEnable = flag;
+                if (flag)
+                    VisualMaterial.SetColor("_BaseColor", EnableColor);
+                else
+                    VisualMaterial.SetColor("_BaseColor", DisableColor);
             }
 
-            if(Input.GetMouseButtonDown(0) && isEnable)
+            if (Input.GetMouseButtonDown(0) && isEnable)
             {
-                var position = FensePoleVisual.position;
-                position.y = hit.point.y;
+                var position = new Vector3(FensePoleVisual.position.x, hit.point.y, FensePoleVisual.position.z);
                 var pole = Instantiate(FensePole, position, Quaternion.identity);
                 poleTrans.Add(pole);
                 switch (poleCount)
                 {
                     case 0:
-                        if(!isCollid)
-                            lenLimit = LengthLimit;
-                        preCropsEntity = Instantiate(CropsParent, position, Quaternion.identity);
                         ++poleCount;
+                        if (isConnect)
+                            ++connectCount;
+                        else
+                            preCropsEntity = Instantiate(CropsParent, position, Quaternion.identity);
+                        FenseCollider.transform.parent = null;
                         break;
                     case 1:
                     case 2:
@@ -93,15 +112,13 @@ public class FenseGenerator : MonoBehaviour
 
     private bool IsBelowLimit(Vector3 prevPos, Vector3 nextPos)
     {
-        return (prevPos - nextPos).sqrMagnitude < lenLimit;
+        return (prevPos - nextPos).sqrMagnitude < lengthLimit;
     }
 
     private void GenerateFense(Transform start, Transform end)
     {
         var distance = Vector3.Distance(start.position, end.position);
         var rateScale = 1f / distance;
-        var partScale = Vector3.Distance(Vector3.Lerp(start.position, end.position, rateScale), start.position);
-        Debug.Log(partScale);
         for (float rate = rateScale; rate < 1; rate += rateScale)
         {
             var position = Vector3.Lerp(start.position, end.position, rate);
@@ -184,20 +201,54 @@ public class FenseGenerator : MonoBehaviour
             ++odd;
         }
 
-        /* Test Code */
-        var playerAI = Instantiate(TestPlayerAI, new Vector3(2, 0, 2), Quaternion.identity);
-        preCropsEntity.Initialize(CropsDatas[0], CropsTrans);
-        preCropsEntity.SetWorking(playerAI, true);
-        preCropsEntity.enabled = true;
-        Debug.Log(CropsTrans.Count);
+        var verts = new Vector3[8];
+        var tris = new int[] { 0, 1, 5, 0, 4, 5, 1, 2, 6, 1, 5, 6, 2, 3, 7, 2, 6, 7,
+                               3, 0, 4, 3, 4, 7, 4, 5, 6, 4, 7, 6, 0, 1, 2, 0, 3, 2};
+        for (int i = 0; i < 4; i++)
+        {
+            verts[i] = poleTrans[i].position - poleTrans[0].position;
+            verts[i + 4] = poleTrans[i].position - poleTrans[0].position + Vector3.up;
+        }
+        var mesh = new Mesh { vertices = verts, triangles = tris };
+        var meshCollider = preCropsEntity.GetComponent<MeshCollider>();
+
+        /* 최적화 필요 */
+        if (isConnect)
+        {
+            var position = preCropsEntity.transform.position;
+            preCropsEntity.transform.position = Vector3.zero;
+            preCropsEntity.AddCrops(CropsTrans);
+            var combineMesh = new Mesh();
+            var combine = new CombineInstance[2];
+            combine[0].mesh = mesh;
+            combine[0].transform = poleTrans[0].transform.localToWorldMatrix;
+            combine[1].mesh = meshCollider.sharedMesh;
+            combine[1].transform = meshCollider.transform.localToWorldMatrix;
+            combineMesh.CombineMeshes(combine);
+            meshCollider.sharedMesh = combineMesh;
+            preCropsEntity.transform.position = position;
+        }
+        else
+        {
+            var playerAI = Instantiate(TestPlayerAI, new Vector3(2, 0, 2), Quaternion.identity);
+            preCropsEntity.Initialize(CropsDatas[0], CropsTrans);
+            preCropsEntity.SetWorking(playerAI, true);
+            preCropsEntity.enabled = true;
+            meshCollider.sharedMesh = mesh;
+        }
     }
 
     private void ClearFense()
     {
+        lengthLimit = DefaultLengthLimit;
+        colliderScale.z = colliderScale.x;
+        FenseCollider.transform.parent = transform;
+        FenseCollider.transform.localScale = colliderScale;
+        FenseCollider.transform.localPosition = Vector3.zero;
         poleTrans.Clear();
         poleCount = 0;
         isEnable = true;
-        isCollid = false;
+        isConnect = false;
     }
 
     private bool CalculateEquation(float p1X, float p1Z, float p2X, float p2Z, float x, float z, ref int cross)
@@ -215,19 +266,24 @@ public class FenseGenerator : MonoBehaviour
         return false;
     }
 
+    private float CalculateAngle(Vector3 from, Vector3 center, Vector3 to)
+    {
+        return Vector3.Angle(from - center, to - center);
+    }
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("FensePole") && !isCollid)
+        if (other.CompareTag("FensePole"))
         {
-            isCollid = true;
+            isConnect = true;
             FensePoleVisual.SetParent(null);
             FensePoleVisual.position = other.transform.position + Vector3.up;
 
             var entity = other.transform.parent.GetComponent<CropsEntity>();
             if (entity && entity.enabled && poleCount.Equals(0))
             {
-                lenLimit = LengthLimit - (entity.CropsCount * 4.84f);
-                collidCropsEntity = entity;
+                preCropsEntity = entity;
+                lengthLimit = DefaultLengthLimit - (entity.CropsCount * 4.84f);
             }
         }
     }
@@ -236,14 +292,14 @@ public class FenseGenerator : MonoBehaviour
     {
         if (other.CompareTag("FensePole"))
         {
-            isCollid = false;
+            isConnect = false;
             FensePoleVisual.SetParent(transform);
             FensePoleVisual.localPosition = Vector3.up;
 
-            if(poleCount.Equals(0))
+            if (poleCount.Equals(0))
             {
-                lenLimit = LengthLimit;
-                collidCropsEntity = null;
+                preCropsEntity = null;
+                lengthLimit = DefaultLengthLimit;
             }
         }
     }
