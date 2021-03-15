3614114d76c655aab8a4e30dceb03f1da292f482
skeleton625 <chaos0824@naver.com>
2021-03-15 19:04:58 +0900

    충돌체 확인 컴포넌트 구현

---
+++ Assets/Scripts/ObjectCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollider : MonoBehaviour
+{
+    [SerializeField] private string CollidTag = "";
+
+    public bool IsUnCollid { get; private set; }
+
+    private void OnEnable()
+    {
+        IsUnCollid = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(CollidTag))
+            IsUnCollid = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(CollidTag))
+            IsUnCollid = true;
+    }
+}
