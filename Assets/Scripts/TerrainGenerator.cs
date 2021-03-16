f011d0dc376dd471c38d80b5e549541fd55062ff
skeleton625 <chaos0824@naver.com>
2021-03-16 19:05:40 +0900

    임시 Terrain Generator 구현

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private Terrain mainTerrain = null;
+    private float[,,] layers = null;
+
+    private void Awake()
+    {
+        mainTerrain = GetComponent<Terrain>();
+        layers = new float[mainTerrain.terrainData.heightmapResolution - 1,
+                           mainTerrain.terrainData.heightmapResolution - 1,
+                           mainTerrain.terrainData.alphamapLayers];
+    }
+
+    public void SetTerrainLayer(int x, int z, int type)
+    {
+        layers[z, x, type] = 1;
+    }
+
+    public void ApplyTerrainLayers(int x, int z)
+    {
+        mainTerrain.terrainData.SetAlphamaps(x, z, layers);
+    }
+}
