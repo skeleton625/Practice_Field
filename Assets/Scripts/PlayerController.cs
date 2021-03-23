fb3544b901d6c7324b9482f02fe60e78cf7daa83
skeleton625 <chaos0824@naver.com>
2021-03-23 19:08:42 +0900

    테스트용 Player 컨트롤러 스크립트 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private UIManager uiManager = null;
+    [SerializeField] private TerrainGenerator Generator = null;
+
+    private int hashCode = 0;
+    private Camera mainCamera = null;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 1000f))
+            {
+                if(hit.transform.CompareTag("Building"))
+                {
+                    if (hit.transform.GetHashCode().Equals(hashCode))
+                    {
+                        uiManager.SetActiveButtonWindows(false);
+                        hashCode = 0;
+                    }
+                    else
+                    {
+                        uiManager.SetActiveButtonWindows(true);
+                        hashCode = hit.transform.GetHashCode();
+                    }
+                }
+            }    
+        }
+    }
+}
