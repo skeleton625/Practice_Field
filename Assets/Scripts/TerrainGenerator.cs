9dea844a2352e76bb5b869570e5faf12a4e8c169
skeleton625 <chaos0824@naver.com>
2021-03-23 19:11:29 +0900

    테스트용 Terrain Layer 수정 함수 구현

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -5,26 +5,53 @@ using UnityEngine;
 public class TerrainGenerator : MonoBehaviour
 {
     private Terrain mainTerrain = null;
-    private float[,,] layers = null;
+    private float[,,] terrainLayers = null;
 
     private void Awake()
     {
         mainTerrain = GetComponent<Terrain>();
-        int size = mainTerrain.terrainData.heightmapResolution - 1;
-        layers = new float[size, size, mainTerrain.terrainData.alphamapLayers];
+        int size = mainTerrain.terrainData.alphamapResolution;
+        terrainLayers = new float[size, size, mainTerrain.terrainData.alphamapLayers];
         for (int z = 0; z < size; z++)
             for (int x = 0; x < size; x++)
-                layers[z, x, 0] = 1;
+                terrainLayers[z, x, 0] = 1;
+
+        mainTerrain.terrainData.SetAlphamaps(0, 0, terrainLayers);
     }
 
     public void SetTerrainLayer(int x, int z, int type)
     {
-        layers[z, x, 0] = 0;
-        layers[z, x, type] = 1;
+        terrainLayers[z, x, 0] = 0;
+        terrainLayers[z, x, type] = 1;
     }
 
-    public void ApplyTerrainLayers(int x, int z)
+    public void ApplyTerrainLayers(int sx, int sz, int i, ref float[,] layers)
     {
-        mainTerrain.terrainData.SetAlphamaps(x, z, layers);
+        float[,,] tmpLayers = new float[layers.GetLength(0), layers.GetLength(1), terrainLayers.GetLength(2)];
+
+        for(int z = 0; z < layers.GetLength(0); z++)
+        {
+            for (int x = 0; x < layers.GetLength(1); x++)
+            {
+                if (layers[z, x] > 0)
+                {
+                    terrainLayers[z + sz, x + sx, i] = tmpLayers[z, x, i] = layers[z, x];
+                    terrainLayers[z + sz, x + sx, 0] = tmpLayers[z, x, 0] = 0;
+                }
+                else
+                {
+                    tmpLayers[z, x, i] = terrainLayers[z + sz, x + sx, i];
+                    tmpLayers[z, x, 0] = terrainLayers[z + sz, x + sx, 0];
+                }
+
+            }
+        }    
+
+        mainTerrain.terrainData.SetAlphamaps(sx, sz, tmpLayers);
+    }
+
+    public void ApplyTerrainLayers()
+    {
+        mainTerrain.terrainData.SetAlphamaps(0, 0, terrainLayers);
     }
 }
