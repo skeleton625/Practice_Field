af0178b88820cdc878577831ccc7738bf4f8ca6e
skeleton625 <chaos0824@naver.com>
2021-03-23 19:10:14 +0900

    밭 생성 관련 스크립트 파일 위치 이동

---
+++ Assets/Scripts/Field/FieldData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FieldData
+{
+    public int sx = 0, sz = 0;
+    public float[,,] layers = null;
+
+    public FieldData(int sx, int sz, int xLen, int zLen, int layerCount)
+    {
+        this.sx = sx;
+        this.sz = sz;
+        layers = new float[xLen, zLen, layerCount];
+    }
+}
