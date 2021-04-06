9c8507156613eaba91c83a20e389312335d0d958
skeleton625 <chaos0824@naver.com>
2021-04-06 19:01:51 +0900

    작물 변수 타입 수정

--- Assets/Scripts/Crops/CropsData.cs
+++ Assets/Scripts/Crops/CropsData.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using GPUInstancer;
 
 public enum FarmType { Type1, Type2, Type3, Type4, Type5, None };
 
 [CreateAssetMenu(fileName = "New Field Data", menuName = "Datas/FieldData")]
 public class CropsData : ScriptableObject
 {
-    public Transform Visual = null;
+    public GPUInstancerPrefab Visual = null;
     public FarmType[] MonthType = null;
     public float CropsWorkScale = 1;
     public float CropsMaxHeight = 0;
